621a756578d93970620108b06da1ecf1e173bdc7
stanislaff <stasmen@mail.ru>
2020-10-22 15:24:27 +0300

    Home work 5

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import request
 print('my programm')
 print("I don't no what to write")
-a = 'gghh 66 hhkkiiyyg'
-a.replace('66', '', 1)
-print(a)
+
+
+def time_task(*time, k):
+  k = k + 1
+  print(k, 'упражнений выполнено за', sum(*time) // 60, 'ч.', sum(*time) % 60, 'мин.')
+
+time_list = [17, 15, 45, 38, 23, 21, 23, 10, 18]
+time_task(time_list, 7)
